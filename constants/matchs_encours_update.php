fd8543877c1d2bc8ba48cb274e9a3c1e83b8d0c6
Tuyen Nguyen <tuyen.nguyen.fr@gmail.com>
2023-08-15 18:08:59 +0200

    maj live update

--- constants/matchs_encours_update.php
+++ constants/matchs_encours_update.php
@@ -2,7 +2,7 @@
 
 try {
 $pdo = new PDO('mysql:host=localhost;dbname=superbowl', username: "root", password: "");
-    $statusUpdate= $pdo->prepare('UPDATE matchs SET match_status = "en cours" WHERE match_date < DATE_ADD(CURDATE(), INTERVAL 1 DAY)');
+    $statusUpdate= $pdo->prepare('UPDATE matchs SET match_status = "en cours" WHERE match_date < DATE_ADD(CURDATE(), INTERVAL 1 DAY) AND CONCAT (match_date, " ", start_time) > NOW() ');
         $statusUpdate->execute();
 
         } catch (PDOException $e) {
--- constants/matchs_live.php
+++ constants/matchs_live.php
@@ -3,7 +3,7 @@
 try {
     
 $pdo = new PDO('mysql:host=localhost;dbname=superbowl', username: "root", password: "");
-    $statusUpdate= $pdo->prepare('UPDATE matchs SET match_status = "live" WHERE match_status = "en cours" AND CONCAT (match_date, " ", start_time) > NOW()');
+    $statusUpdate= $pdo->prepare('UPDATE matchs SET match_status = "live" WHERE match_status = "en cours" AND CONCAT (match_date, " ", start_time) < NOW()');
         $statusUpdate->execute();
 
         } catch (PDOException $e) {
