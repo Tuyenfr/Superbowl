f87f351d7178b107619471f4343c650322a8c414
Tuyen Nguyen <tuyen.nguyen.fr@gmail.com>
2023-08-24 20:41:30 +0200

    maj refresh time

--- user/matchs_over_user.php
+++ user/matchs_over_user.php
@@ -7,7 +7,7 @@ session_start();
 <head>
    <meta charset="utf-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <meta http-equiv='refresh' content='20'>
+   <meta http-equiv='refresh' content='30'>
    <title>SuperBowl-BET</title>
    <link rel="stylesheet" href="../CSS/style.css" type="text/css">
 </head>
