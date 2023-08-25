71c1b545f2aa8f43d5e112ba6c34cd65f1b1fb08
Tuyen Nguyen <tuyen.nguyen.fr@gmail.com>
2023-08-25 22:33:23 +0200

    pagination matchtocome

--- user/home.php
+++ user/home.php
@@ -407,12 +407,9 @@ session_start();
                   $nbPages = ceil($count / $itemsPerPage);
 
 
-                  if (isset($_GET["page"])) {
-                     $currentPage = $_GET["page"];
-                     $limitX = ($currentPage * $itemsPerPage) - 10;
-                     $limitY = $itemsPerPage - 1;
+                  if ($count > 10) {
 
-                     foreach ($pdo->query('SELECT * FROM matchs WHERE match_status = "terminé" ORDER BY match_date DESC, start_time DESC LIMIT ' . $limitX . ', ' . $limitY . '', PDO::FETCH_ASSOC) as $match_name) {
+                     foreach ($pdo->query('SELECT * FROM matchs WHERE match_status = "terminé" ORDER BY match_date DESC, start_time DESC LIMIT 0, 9', PDO::FETCH_ASSOC) as $match_name) {
 
                         $date =  $match_name['match_date'];
                         $dateUS = DateTime::createFromFormat('Y-m-d', $date);
@@ -481,124 +478,107 @@ session_start();
                                  </td>
                               </tr>
                            </table>
+                        </div>
+                     <?php } ?>
+
+                     <ul class="pages_li">
+                        <li>
+                           <a class="pages_liens" href="home.php">Page 1 &nbsp</a>
+                        </li>
+                        <?php
+                        for ($i = 2; $i <= $nbPages; $i++) { ?>
+                           <li>
+                              <a class="pages_liens" href='home_following.php?page=<?php echo $i; ?>'><?php echo $i; ?> &nbsp</a>
+                           </li>
+                        <?php } ?>
+                        <li>&nbsp &nbsp &nbsp &nbsp &nbsp</li> <?php
+                                                               echo '</ul>';
+                                                            } else {
+
+                                                               foreach ($pdo->query('SELECT * FROM matchs WHERE match_status = "terminé" ORDER BY match_date DESC, start_time DESC LIMIT 0, 9', PDO::FETCH_ASSOC) as $match_name) {
+
+                                                                  $date =  $match_name['match_date'];
+                                                                  $dateUS = DateTime::createFromFormat('Y-m-d', $date);
+                                                                  $dateUSfull = date_format($dateUS, 'l d F Y');
+                                                                  $dateFRday = str_replace(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'], ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'], $dateUSfull);
+                                                                  $match_dateFR = str_replace(['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'], ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'], $dateFRday);
+
+                                                               ?>
+                           <div>
+                              <table width="100%">
+                                 <tr width="100%">
+                                    <td class="display_td">
+                                       <?php echo $match_dateFR . ' - ' . 'Match' . ' ' . $match_name['match_status']; ?>
+                                    </td>
+                                 </tr>
+
+                                 <tr width="100%">
+                                    <td class="display_td">
+                                       <?php echo substr($match_name['start_time'], 0, -3) . ' - ' . substr($match_name['end_time'], 0, -3); ?>
+                                    </td>
+                                 </tr>
+                              </table>
                            </div>
-                        <?php } ?>
 
-                        <ul class="pages_li">
-                           <li>
-                              <a class="pages_liens" href="home.php">Page 1 &nbsp</a>
-                           </li>
-                           <?php
-                           for ($i = 2; $i <= $nbPages; $i++) { ?>
-                              <li>
-                                 <a class="pages_liens" href='home.php?page=<?php echo $i; ?>'><?php echo $i; ?> &nbsp</a>
-                              </li>
-                              <?php } ?>
-                                 <li>&nbsp &nbsp &nbsp &nbsp &nbsp</li> <?php
-                        echo '</ul>';
-         
-                        echo '<br>';
+                           <div>
+                              <table width="100%">
+                                 <tr>
+                                    <td class="display_teamname" width="48%">
+                                       <?php echo $match_name['team1_name']; ?>
+                                    </td>
 
-                        } else {
+                                    <td class="display_teamname" width="4%">
+                                       /
+                                    </td>
 
-                           foreach ($pdo->query('SELECT * FROM matchs WHERE match_status = "terminé" ORDER BY match_date DESC, start_time DESC LIMIT 0, 9', PDO::FETCH_ASSOC) as $match_name) {
+                                    <td class="display_teamname" width="48%">
+                                       <?php echo $match_name['team2_name']; ?>
+                                    </td>
+                                 </tr>
 
-                              $date =  $match_name['match_date'];
-                              $dateUS = DateTime::createFromFormat('Y-m-d', $date);
-                              $dateUSfull = date_format($dateUS, 'l d F Y');
-                              $dateFRday = str_replace(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'], ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'], $dateUSfull);
-                              $match_dateFR = str_replace(['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'], ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'], $dateFRday);
+                                 <tr>
+                                    <td class="display_betnumber" width="48%">
+                                       Score
+                                    </td>
 
-                     ?>
-                        <div>
-                           <table width="100%">
-                              <tr width="100%">
-                                 <td class="display_td">
-                                    <?php echo $match_dateFR . ' - ' . 'Match' . ' ' . $match_name['match_status']; ?>
-                                 </td>
-                              </tr>
+                                    <td class="display_betnumber" width="4%">
 
-                              <tr width="100%">
-                                 <td class="display_td">
-                                    <?php echo substr($match_name['start_time'], 0, -3) . ' - ' . substr($match_name['end_time'], 0, -3); ?>
-                                 </td>
-                              </tr>
-                           </table>
-                        </div>
+                                    </td>
 
-                        <div>
-                           <table width="100%">
-                              <tr>
-                                 <td class="display_teamname" width="48%">
-                                    <?php echo $match_name['team1_name']; ?>
-                                 </td>
+                                    <td class="display_betnumber" width="48%">
+                                       Score
+                                    </td>
+                                 </tr>
 
-                                 <td class="display_teamname" width="4%">
-                                    /
-                                 </td>
+                                 <tr>
+                                    <td align="center" width="48%">
+                                       <button class="button_score"><?php echo $match_name['team1_score']; ?></button>
+                                    </td>
 
-                                 <td class="display_teamname" width="48%">
-                                    <?php echo $match_name['team2_name']; ?>
-                                 </td>
-                              </tr>
+                                    <td calign="center" width="4%">
 
-                              <tr>
-                                 <td class="display_betnumber" width="48%">
-                                    Score
-                                 </td>
+                                    </td>
 
-                                 <td class="display_betnumber" width="4%">
+                                    <td align="center" width="48%">
+                                       <button class="button_score"><?php echo $match_name['team2_score']; ?></button>
+                                    </td>
+                                 </tr>
+                              </table>
+                           </div>
+                  <?php }
+                                                            }
+                                                         } catch (PDOException $e) {
+                                                            echo 'pb de connexion';
+                                                         }
 
-                                 </td>
-
-                                 <td class="display_betnumber" width="48%">
-                                    Score
-                                 </td>
-                              </tr>
-
-                              <tr>
-                                 <td align="center" width="48%">
-                                    <button class="button_score"><?php echo $match_name['team1_score']; ?></button>
-                                 </td>
-
-                                 <td calign="center" width="4%">
-
-                                 </td>
-
-                                 <td align="center" width="48%">
-                                    <button class="button_score"><?php echo $match_name['team2_score']; ?></button>
-                                 </td>
-                              </tr>
-                           </table>
-                        </div>
-                        <?php } ?>
-
-                        <ul class="pages_li">
-                           <li>
-                              <a class="pages_liens" href="home.php">Page 1 &nbsp</a>
-                           </li>
-                           <?php
-                           for ($i = 2; $i <= $nbPages; $i++) { ?>
-                           <li>
-                                 <a class="pages_liens" href='home.php?page=<?php echo $i; ?>'><?php echo $i; ?> &nbsp</a>
-                           </li>
-                           <?php } ?>
-                                 <li>&nbsp &nbsp &nbsp &nbsp &nbsp</li> <?php
-                        echo '</ul>';
-               
-                        }
-                     } catch (PDOException $e) {
-                        echo 'pb de connexion';
-                     }
-
-               ?>
+                  ?>
 
             </div>
 
          </div>
 
          <div class="aside_right">
-               <?php include_once "../templates/aside_right_content.php"; ?>
+            <?php include_once "../templates/aside_right_content.php"; ?>
          </div>
 
       </section>
