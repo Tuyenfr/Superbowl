fe01077ebe9045599e5c66f18dfa5ec9527c509f
Tuyen Nguyen <tuyen.nguyen.fr@gmail.com>
2023-09-07 21:49:22 +0200

    pdo planethoster

--- constants/pdo.php
+++ constants/pdo.php
@@ -1,4 +1,5 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;dbname=superbowl', 'root', '');
+/**$pdo = new PDO('mysql:host=localhost;dbname=superbowl', 'root', '');**/
+$pdo = new PDO('mysql:host=localhost; dbname=yngbgcth_superbowl', 'yngbgcth_admin2', '!+JJJ2ZeJJ6!((J(ec');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
          
