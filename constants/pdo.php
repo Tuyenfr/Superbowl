a85b87daaff162e843255bcfb5214ea6f69552f8
Tuyen Nguyen <tuyen.nguyen.fr@gmail.com>
2023-09-07 23:46:57 +0200

    array setname utf-8

--- constants/pdo.php
+++ constants/pdo.php
@@ -1,5 +1,7 @@
 <?php
+
 /**$pdo = new PDO('mysql:host=localhost;dbname=superbowl', 'root', '');**/
-$pdo = new PDO('mysql:host=localhost; dbname=yngbgcth_superbowl', 'yngbgcth_admin2', '!+JJJ2ZeJJ6!((J(ec');
+
+$pdo = new PDO('mysql:host=localhost; dbname=yngbgcth_superbowl', 'yngbgcth_admin2', '!+JJJ2ZeJJ6!((J(ec', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
          
