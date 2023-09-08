344b91b3b66da064d556694f6e6cc00f5dba9e8f
Tuyen Nguyen <tuyen.nguyen.fr@gmail.com>
2023-09-08 09:56:02 +0200

    favicon

--- templates/header_index.php
+++ templates/header_index.php
@@ -8,6 +8,12 @@ session_start();
    <meta charset="utf-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <meta http-equiv='refresh' content='60'>
+
+   <link rel="apple-touch-icon" sizes="180x180" href="./Images/favicon/apple-touch-icon.png">
+   <link rel="icon" type="image/png" sizes="32x32" href="./Images/favicon/favicon-32x32.png">
+   <link rel="icon" type="image/png" sizes="16x16" href="./Images/favicon/favicon-16x16.png">
+   <link rel="manifest" href="./Images/favicon/site.webmanifest">
+
    <title>SuperBowl-BET - Pari en ligne</title>
    <link rel="stylesheet" href="./CSS/style.css" type="text/css">
 </head>
