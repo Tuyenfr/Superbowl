05a55ebd8196b66037d126b418b20557276a7d55
Tuyen Nguyen <tuyen.nguyen.fr@gmail.com>
2023-09-08 22:27:41 +0200

    format image

--- user/about_superbowl_user.php
+++ user/about_superbowl_user.php
@@ -31,7 +31,7 @@ session_start();
 
       <div class="container_useraccount">
 
-      <div style="text-align: center; padding-top: 10px"><img width="35%" src="../Images/superbowl-LVII.avif" alt="photo Super Bowl"></div>
+      <div style="text-align: center; padding-top: 10px"><img width="35%" src="../Images/superbowl-LVII.png" alt="photo Super Bowl"></div>
                
          <p class="subtitles_aboutSB" id="superbowl_history">Histoire du Super Bowl</p>
 
