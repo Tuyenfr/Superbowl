0437f642d9ed654845e33a44ddc350c54ae25d39
Tuyen Nguyen <tuyen.nguyen.fr@gmail.com>
2023-09-19 16:34:47 +0200

    update pdo

--- constants/pdo.php
+++ constants/pdo.php
@@ -3,8 +3,8 @@
 /** -- Connection locale -- **/
 $pdo = new PDO('mysql:host=localhost;dbname=superbowl', 'root', '');
 
-/** -- Connection Planethoster -- */
-/**$pdo = new PDO('mysql:host=localhost; dbname=yngbgcth_superbowl', 'yngbgcth_admin2', '', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));**/
+/** -- Connection hébergeur -- */
+/**$pdo = new PDO('mysql:host=localhost; dbname=XXXX_superbowl', 'XXXX_XXXX', 'XXXX', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));**/
 
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
          
